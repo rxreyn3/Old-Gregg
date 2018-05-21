8fc03a82edb343c43a3723df31e8f5ab73f8149f
rxreyn3 <rxreyn3@gmail.com>
2018-05-21 16:34:51 -0600

    Added test cat.

--- routes/standup.js
+++ routes/standup.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function (req, res) {
-    res.send('POST request to the homepage')
+    let data = { 
+        response_type: 'in_channel', // public to the channel 
+        text: '302: Found', 
+        attachments:[ { 
+          image_url: 'https://http.cat/302.jpg' 
+        } ]}; 
+    res.json(data);
 })
 
 module.exports = router;
