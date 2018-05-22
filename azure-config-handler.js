40b879541f676fb6edaf78ce7687e5091e4cb9f4
thevoid\deadpool <ryan.reynolds@thevoid.com>
2018-05-22 17:58:26 -0600

    Initial commit of blob storage.

---
+++ azure-config-handler.js
@@ -0,0 +1,55 @@
+var storage = require('azure-storage')
+var path = require('path')
+
+var blobService = storage.createBlobService()
+var containerName = 'gregg-container'
+var sourceFilePath = path.resolve('config/teams.json')
+var blobName = path.basename(sourceFilePath, path.extname(sourceFilePath))
+
+exports.createContainer = function () {
+  return new Promise((resolve, reject) => {
+    blobService.createContainerIfNotExists(containerName, { publicAccessLevel: 'blob' }, err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({ message: `Container '${containerName}' created` })
+      }
+    })
+  })
+}
+
+exports.upload = function () {
+  return new Promise((resolve, reject) => {
+    blobService.createBlockBlobFromLocalFile(containerName, blobName, sourceFilePath, err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({ message: `Upload of '${blobName}' complete` })
+      }
+    })
+  })
+}
+
+exports.download = function () {
+  return new Promise((resolve, reject) => {
+    blobService.getBlobToLocalFile(containerName, blobName, sourceFilePath, err => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({ message: `Download of '${blobName}' complete` })
+      }
+    })
+  })
+}
+
+exports.list = function () {
+  return new Promise((resolve, reject) => {
+    blobService.listBlobsSegmented(containerName, null, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({ message: `Items in container '${containerName}':`, data: data })
+      }
+    })
+  })
+}
--- routes/slashcommand.js
+++ routes/slashcommand.js
@@ -1,14 +1,30 @@
 var express = require('express')
 var router = express.Router()
+var azure = require('../azure-config-handler')
 
 router.post('/', function (req, res) {
-  var data = {
+  if (req.body.text === 'init') {
+    azure.list().then(response => {
+      if (response.data.entries.length > 0 && response.data.entries[0].name === 'teams') {
+        res.send('Team already exists.')
+      } else {
+        res.send('Team does not exist, creating it now.')
+        azure.upload()
+      }
+    }).catch(reason => {
+      res.send(reason)
+    })
+  } else {
+    res.json(req.body)
+  }
+
+  /*   var data = {
     response_type: 'in_channel', // public to the channel
     text: '302: Found',
     attachments: [ {
       image_url: 'https://http.cat/302.jpg'
     } ]}
-  res.json(data)
+  res.json(data) */
 })
 
 module.exports = router
