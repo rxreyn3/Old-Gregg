cbfb4f7230bf434ace2b98364c895282aa484a6b
rxreyn3 <rxreyn3@gmail.com>
2018-05-21 21:12:22 -0600

    Added first web handle call.

--- slack-event-handler.js
+++ slack-event-handler.js
@@ -1,3 +1,8 @@
+var { WebClient } = require('@slack/client');
+
+var token = process.env.SLACK_TOKEN;
+var web = new WebClient(token);
+
 var createSlackEventAdapter = require('@slack/events-api').createSlackEventAdapter;
 var slackEvents = createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN);
 
@@ -5,7 +10,18 @@ exports.getEventHandler = function () {
     return slackEvents.expressMiddleware();
 }
 
-slackEvents.on('message', (event)=> {
-  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+slackEvents.on('message', (event) => {
+    console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+    if(event.user == undefined) return;
+    web.chat.postMessage({
+            channel: event.channel,
+            text: 'Hello my little man peach.'
+        })
+        .then((res) => {
+            // `res` contains information about the posted message
+            console.log('Message sent: ', res.ts);
+        })
+        .catch(console.error);
 });
+
 slackEvents.on('error', console.error);
